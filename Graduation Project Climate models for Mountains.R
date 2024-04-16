8746f8e5b075c40c4a7780933b591cfa879a9ef2
RemkoPape <remkopape@gmail.com>
2024-04-16 10:12:32 +0200

    added data DEm and GMBAVariables

--- Graduation Project Climate models for Mountains.R
+++ Graduation Project Climate models for Mountains.R
@@ -18,8 +18,16 @@ library("terra")
 library("sf")
 library("raster")
 
-wd <- ("C:/Users/repap5991/Downloads/Data/R/Rdocs")
+wd <- ("C:/Users/repap5991/Downloads/Data/R/Rdocs/")
 setwd(wd) #setting the working directory
 
 # 1.0 loading data ---------
+
+DEM <- raster(paste0(wd, "DataStorage/DEM/dem_geo.sdat"))  ## adding dem_geo chelsa dem
+
+GMBApoly <- st_read(paste0(wd, "DataStorage/ArcGIs Data/GMBA_Level4_ExportTable_TableToExcel.csv"))  ## arcgis dataset which contains all the variables according to the GMBA_level4.
+
+
+
+
 
