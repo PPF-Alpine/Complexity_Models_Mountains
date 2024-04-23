df52b76beb98552a47370b05fa71c0476db71b9b
RemkoPape <remkopape@gmail.com>
2024-04-23 14:16:10 +0200

    addition of PCA plot for the different mountain variables

--- Graduation Project Climate models for Mountains.R
+++ Graduation Project Climate models for Mountains.R
@@ -8,16 +8,15 @@ rm(list = ls()) #full reset
 ## general
 library("tidyverse")    #activating the package for running code for it.
 library("plyr")
-
 library("ReacTran")
 library("fields")
 library("animation")
-
 ## project specific
+library("terra") #for working with rasters
+library("sf") #for working with rasters
+library("raster") #for working with rasters
+library("factoextra")  # For PCA visualization
 
-library("terra")
-library("sf")
-library("raster")
 
 wd <- ("C:/Users/repap5991/Downloads/Data/R/Rdocs/")
 setwd(wd) #setting the working directory
@@ -38,7 +37,7 @@ Ama_dy <- raster(paste0(wd, "DataStorage/Amatulli/dtm_dy_merit.dem_m_250m_s0..0c
 Ama_dyy <- raster(paste0(wd, "DataStorage/Amatulli/dtm_dyy_merit.dem_m_250m_s0..0cm_2018_v1.0.tif"))
 Ama_slope <- raster(paste0(wd, "DataStorage/Amatulli/dtm_slope_merit.dem_m_250m_s0..0cm_2018_v1.0.tif"))
 Ama_vrm <- raster(paste0(wd, "DataStorage/Amatulli/dtm_vrm_merit.dem_m_250m_s0..0cm_2018_v1.0.tif"))
-
+DEM <- raster(paste0(wd, "DataStorage/DEM/Chelsa V2/dem_latlong.sdat"))
 Ham_Landfrom <- raster(paste0(wd, "DataStorage/NewHammond/World Ecological Facets L.tif"))
 Saph_litho <- raster(paste0(wd, "DataStorage/Saphre/dtm_lithology_usgs.ecotapestry_c_250m_s0..0cm_2014_v1.0.tif"))
 
@@ -47,67 +46,57 @@ Saph_litho <- raster(paste0(wd, "DataStorage/Saphre/dtm_lithology_usgs.ecotapest
 
 str(GMBApoly) ## overview of data
 
-numeric_dataconversion <- c(4:30, 32, 34:36)
-Catagorical_dataconversion <- c(31, 33, 3, 2)
+numeric_Data <- c(4:19, 23, 25:41) 
+GMBApoly[numeric_Data] <- lapply(GMBApoly[numeric_Data], as.numeric) #set variables as numeric
+
+Catagorical_data <- c(2, 20, 21, 22, 24)
+GMBApoly[Catagorical_data] <- lapply(GMBApoly[Catagorical_data], as.factor) #set variables as factor
 
 
-GMBApoly[numeric_dataconversion] <- lapply(GMBApoly[numeric_dataconversion], as.numeric)
-GMBApoly[Catagorical_dataconversion] <- lapply(GMBApoly[Catagorical_dataconversion], as.factor)
 
 
 # 3.0 Basic statistics and overview---------------------------------------------------
-
-
 ## 3.1 Set up a multi-panel plot for orginal data--------------------------------------------------------
 
-
-par(mfrow=c(4, 2), mar=c(3,3,1,1))
-
-
-hist(Ama_dx, main = "Histogram of dx", xlab = "dx")
-hist(Ama_dxx, main = "Histogram of dxx", xlab = "dxx")
-hist(Ama_dy, main = "Histogram of dy", xlab = "dy")
-hist(Ama_dyy, main = "Histogram of dyy", xlab = "dyy")
-hist(Ama_slope, main = "Histogram of slope", xlab = "slope")
-hist(Ama_vrm, main = "Histogram of vrm", xlab = "vrm")
-
-hist(Ham_Landfrom, main = "Histogram of Landforms", xlab = "Landforms")
-hist(Saph_litho, main = "Histogram of LithoClass", xlab = "Lithclasses")
-
+# Define colors for histograms
+hist_colors <- c("#1f77b4", "#ff7f0e", "#2ca02c", "#d62728", "#9467bd", "#8c564b", "#e377c2", "#7f7f7f", "#17becf")
 
 # Create a list of raster datasets
 raster_datasets <- list(
-  Ama_dx = list(title = "Histogram of dx", x_label = "dx"),
-  Ama_dxx = list(title = "Histogram of dxx", x_label = "dxx"),
-  Ama_dy = list(title = "Histogram of dy", x_label = "dy"),
-  Ama_dyy = list(title = "Histogram of dyy", x_label = "dyy"),
-  Ama_slope = list(title = "Histogram of slope", x_label = "slope"),
-  Ama_vrm = list(title = "Histogram of vrm", x_label = "vrm"),
-  Ham_Landfrom = list(title = "Histogram of Landforms", x_label = "Landforms"),
-  Saph_litho = list(title = "Histogram of LithoClass", x_label = "Lithclasses")
+  Ama_dx = "dx",
+  Ama_dxx = "dxx",
+  Ama_dy = "dy",
+  Ama_dyy = "dyy",
+  Ama_slope = "slope",
+  Ama_vrm = "vrm",
+  Ham_Landfrom = "Landforms",
+  Saph_litho = "LithoClass",
+  DEM = "DEM"
 )
 
 # Set up the plotting layout
-par(mfrow=c(4, 2), mar=c(3,3,1,1))
+par(mfrow=c(5, 2), mar=c(3,3,1,1))
 
 # Generate histograms for each dataset using ggplot2
-for (name in names(raster_datasets)) {
-  raster_data <- get(name)
-  values <- raster_data[]
-  ggplot(data.frame(Data = values), aes(x = Data)) +
-    geom_histogram(binwidth = 1, color = "black") +
-    labs(x = raster_datasets[[name]]$x_label, y = "Frequency", title = raster_datasets[[name]]$title)
+for (i in seq_along(raster_datasets)) {
+  dataset <- raster_datasets[[i]]
+  raster_data <- get(names(raster_datasets)[i])
+  
+  hist(raster_data, col = hist_colors[i], main = paste("Histogram of", dataset), xlab = "", ylab = "Frequency")
 }
+
+
 ## 3.2  Plot histograms of GMBApoly data--------------------------------------------------------
 
 print(names(GMBApoly)[variablesHis])
 
-
-
-Vari_Num <- c(4, 6, 19:30, 35, 36) # Define the column indices of interest Max, mean, uplift, area
+Vari_Num <- c(4, 6, 20:33, 38, 39) # Define the column indices of interest Max, mean, uplift, area
 Title_var_Num <- names(GMBApoly)[Vari_Num] # Extract the column names of interest
+
+
+
 # Create histograms for each variable
-par(mfrow = c(4, 4))  # Set up 4x4 layout for multiple plots
+par(mfrow = c(5, 4))  # Set up 4x4 layout for multiple plots
 
 for (i in seq_along(Vari_Num)) {
   hist(GMBApoly[[Vari_Num[i]]], 
@@ -122,18 +111,15 @@ for (i in seq_along(Vari_Num)) {
        yaxt = "s"  # Show y-axis labels
   )
   box()  # Add box around each plot
-  abline(v = mean(GMBApoly[[variablesHis[i]]]), col = "red", lwd = 2)  # Add vertical line for mean
-  abline(v = median(GMBApoly[[variablesHis[i]]]), col = "blue", lwd = 2, lty = 2)  # Add vertical line for median
+  abline(v = mean(GMBApoly[[Vari_Num[i]]]), col = "red", lwd = 2)  # Add vertical line for mean
+  abline(v = median(GMBApoly[[Vari_Num[i]]]), col = "blue", lwd = 2, lty = 2)  # Add vertical line for median
 }
 
 
+
 ## 3,3 Violin plots of GMBApoly data --------------------------------------------------------
-
-
 # Reshape the data to long format
 melted_data <- gather(GMBApoly, key = "Variable", value = "Value", all_of(Vari_Num))
-
-
 melted_data$variable <- factor(melted_data$variable, levels = Vari_Num)
 
 # Create violin plots with facets and add a boxplot
@@ -143,10 +129,28 @@ ggplot(melted_data, aes(x = Variable, y = Value, fill = Variable)) +
   facet_wrap(~ Variable, scales = "free", ncol = 4) +
   xlab("Variable") +
   ylab("Value") +
-  ggtitle("Violin Plots of All Numeric Variables with Boxplot") +
+  ggtitle("Violin Plots of GNBApoly All Numeric Variables with Boxplot") +
   scale_fill_manual(values = rep(rainbow(length(Title_var_Num)), each = 2)) +
   theme_minimal() +
   theme(legend.direction = "vertical", legend.box = "vertical", legend.key.size = unit(0.5, "cm"))
+
+
+
+
+## 3.4 summarizing results PCA
+
+
+PCAData <- GMBApoly[,c(4, 6, 33:41)] #taking only the numeric values 
+
+res.pca <- prcomp(PCAData, scale = TRUE) #rescaled variables, which makes that they're scaled according to eachother and limiting effect of large ranges compared with smaller.
+
+if (anyNA(PCAData)) {
+  PCAData <- na.omit(PCAData)  # Remove rows with missing values, as the PCA doesn't work with that
+}
+
+# Visualize PCA results
+pca_plot <- fviz_pca(res.pca, geom = "point", ggtheme = theme_minimal())
+
 
 
 
