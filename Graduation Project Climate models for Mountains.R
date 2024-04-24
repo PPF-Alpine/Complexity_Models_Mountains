0f0e135422e2ac5f085107ad62fa7fa745846e64
RemkoPape <remkopape@gmail.com>
2024-04-24 16:24:05 +0200

    Addition of Chelsa variables past 21000, present 1981-2010. Also added histograms to visualize these datasets

--- Graduation Project Climate models for Mountains.R
+++ Graduation Project Climate models for Mountains.R
@@ -1,11 +1,12 @@
-# 0 setup ----------------------------------------------------------------------
+# 0 setup ------------------------------------------------------------------------------------------------------------------------------------------
 
 rm(list = ls()) #full reset
+rm(list = grep("^Temp_|^Prec_", ls(), value = TRUE))
 
 #install.packages("dplyr") # installing packages
 #install.packages( c( "dplyr", "ggplot2", "plyr" ) )
 
-## general
+## general 
 library("tidyverse")    #activating the package for running code for it.
 library("plyr")
 library("ReacTran")
@@ -17,19 +18,15 @@ library("sf") #for working with rasters
 library("raster") #for working with rasters
 library("factoextra")  # For PCA visualization
 
-
 wd <- ("C:/Users/repap5991/Downloads/Data/R/Rdocs/")
 setwd(wd) #setting the working directory
 
-# 1.0 loading data -----------------------------------------------------------------
+# 1.0 loading data -------------------------------------------------------------------------------------------------------------------------------------
 
-#DEM <- raster(paste0(wd, "DataStorage/DEM/dem_geo.sdat"))  ## adding dem_geo chelsa dem
-
+## 1.1 GMBA ArcGIS polygon load  -----------------------------------------------------------------------------------------------------------------
 GMBApoly <- st_read(paste0(wd, "DataStorage/ArcGIs Data/GMBA_Level4_ExportTable_TableToExcel.csv"))  ## arcgis dataset which contains all the variables according to the GMBA_level4.
 
-desired_order <- c(1, 5, 2, 3, 4, 6, 7) # Define the desired order for the first columns
-GMBApoly <- GMBApoly[, c(desired_order, setdiff(order(names(GMBApoly)), desired_order))] # Reorder the columns in the GMBApoly dataframe
-
+## 1.2 orginal geomorp variables   -----------------------------------------------------------------------------------------------------------------
 
 Ama_dx <- raster(paste0(wd, "DataStorage/Amatulli/dtm_dx_merit.dem_m_250m_s0..0cm_2018_v1.0.tif")) #Original datasets
 Ama_dxx <- raster(paste0(wd, "DataStorage/Amatulli/dtm_dxx_merit.dem_m_250m_s0..0cm_2018_v1.0.tif"))
@@ -41,8 +38,36 @@ DEM <- raster(paste0(wd, "DataStorage/DEM/Chelsa V2/dem_latlong.sdat"))
 Ham_Landfrom <- raster(paste0(wd, "DataStorage/NewHammond/World Ecological Facets L.tif"))
 Saph_litho <- raster(paste0(wd, "DataStorage/Saphre/dtm_lithology_usgs.ecotapestry_c_250m_s0..0cm_2014_v1.0.tif"))
 
+## 1.3 chelsa climatic variables   -----------------------------------------------------------------------------------------------------------------
 
-# 2.0 data type fixing --------------------------------------------------------------
+Prec_CCSM_Past <- raster(paste0(wd, "DataStorage/Chelsa/Prec Bio Past/CHELSA_PMIP_CCSM4_BIO_12.tif")) #BIO 12 is the annual precipitation amount kg m-2, scale 0.1
+Prec_CNRM_Past <- raster(paste0(wd, "DataStorage/Chelsa/Prec Bio Past/CHELSA_PMIP_CNRM-CM5_BIO_12.tif")) #Accumulated precipitation amount over 1 year
+Prec_FGOA_Past <- raster(paste0(wd, "DataStorage/Chelsa/Prec Bio Past/CHELSA_PMIP_FGOALS-g2_BIO_12.tif")) # in the past LGM 21000 YR
+Prec_IPSL_Past <- raster(paste0(wd, "DataStorage/Chelsa/Prec Bio Past/CHELSA_PMIP_IPSL-CM5A-LR_BIO_12.tif")) 
+Prec_MIRO_Past <- raster(paste0(wd, "DataStorage/Chelsa/Prec Bio Past/CHELSA_PMIP_MIROC-ESM_BIO_12.tif")) 
+Prec_MPIE_Past <- raster(paste0(wd, "DataStorage/Chelsa/Prec Bio Past/CHELSA_PMIP_MPI-ESM-P_BIO_12.tif")) 
+Prec_MRIC_Past <- raster(paste0(wd, "DataStorage/Chelsa/Prec Bio Past/CHELSA_PMIP_MRI-CGCM3_BIO_12.tif")) 
+
+Prec_Pres <- raster(paste0(wd, "DataStorage/Chelsa/Prec Bio Present/CHELSA_bio12_1981-2010_V.2.1.tif")) #BIO 12 is the annual precipitation amount kg m-2, scale 0.1 Accumulated precipitation amount over 1 year in the Present 1981 - 2010
+
+
+Temp_CCSM_Past <- raster(paste0(wd, "DataStorage/Chelsa/Temp Bio Past/CHELSA_PMIP_CCSM4_BIO_01.tif")) #BIO 01 is the mean annual temperature °C, scale 0.1, -273.15 offset
+Temp_CNRM_Past <- raster(paste0(wd, "DataStorage/Chelsa/Temp Bio Past/CHELSA_PMIP_CNRM-CM5_BIO_01.tif")) #Accumulated precipitation amount over 1 year
+Temp_FGOA_Past <- raster(paste0(wd, "DataStorage/Chelsa/Temp Bio Past/CHELSA_PMIP_FGOALS-g2_BIO_01.tif")) # in the past LGM 21000 YR
+Temp_IPSL_Past <- raster(paste0(wd, "DataStorage/Chelsa/Temp Bio Past/CHELSA_PMIP_IPSL-CM5A-LR_BIO_01.tif"))
+Temp_MIRO_Past <- raster(paste0(wd, "DataStorage/Chelsa/Temp Bio Past/CHELSA_PMIP_MIROC-ESM_BIO_01.tif"))
+Temp_MPIE_Past <- raster(paste0(wd, "DataStorage/Chelsa/Temp Bio Past/CHELSA_PMIP_MPI-ESM-P_BIO_01.tif"))
+Temp_MRIC_Past <- raster(paste0(wd, "DataStorage/Chelsa/Temp Bio Past/CHELSA_PMIP_MRI-CGCM3_BIO_01.tif"))
+
+Temp_Pres <- raster(paste0(wd, "DataStorage/Chelsa/Temp Bio Present/CHELSA_bio1_1981-2010_V.2.1.tif")) #BIO 01 is the mean annual temperature °C, scale 0.1, offset, -273.15 Accumulated Temperature amount over 1 year in the Present 1981 - 2010.
+
+
+# 2.0 data type fixing ----------------------------------------------------------------------------------------------------------------------------------
+
+## 2.1 GMBApoly  ----------------------------------------------------------------------------------------------------------------------------------
+
+desired_order <- c(1, 5, 2, 3, 4, 6, 7) # Define the desired order for the first columns
+GMBApoly <- GMBApoly[, c(desired_order, setdiff(order(names(GMBApoly)), desired_order))] # Reorder the columns in the GMBApoly dataframe
 
 str(GMBApoly) ## overview of data
 
@@ -55,25 +80,24 @@ GMBApoly[Catagorical_data] <- lapply(GMBApoly[Catagorical_data], as.factor) #set
 
 
 
-# 3.0 Basic statistics and overview---------------------------------------------------
-## 3.1 Set up a multi-panel plot for orginal data--------------------------------------------------------
+## 2.2                ----------------------------------------------------------------------------------------------------------------------------------
+
+## 2.3 CHELSA data  ----------------------------------------------------------------------------------------------------------------------------------
+
+
+
+# 3.0 Basic statistics and overview-----------------------------------------------------------------------------------------------------------------------
+
+## 3.1 Set up a multi-panel plot for original data--------------------------------------------------------
 
 # Define colors for histograms
-hist_colors <- c("#1f77b4", "#ff7f0e", "#2ca02c", "#d62728", "#9467bd", "#8c564b", "#e377c2", "#7f7f7f", "#17becf")
-
-# Create a list of raster datasets
+hist_colors <- c("#1f77b4", "#ff7f0e", "#2ca02c", "#d62728", "#9467bd", 
+                 "#8c564b", "#e377c2", "#7f7f7f", "#17becf")
+# Create a list of original raster data sets
 raster_datasets <- list(
-  Ama_dx = "dx",
-  Ama_dxx = "dxx",
-  Ama_dy = "dy",
-  Ama_dyy = "dyy",
-  Ama_slope = "slope",
-  Ama_vrm = "vrm",
-  Ham_Landfrom = "Landforms",
-  Saph_litho = "LithoClass",
-  DEM = "DEM"
+  Ama_dx = "dx", Ama_dxx = "dxx", Ama_dy = "dy", Ama_dyy = "dyy", Ama_slope = "slope",
+  Ama_vrm = "vrm", Ham_Landfrom = "Landforms", Saph_litho = "LithoClass", DEM = "DEM"
 )
-
 # Set up the plotting layout
 par(mfrow=c(5, 2), mar=c(3,3,1,1))
 
@@ -85,12 +109,74 @@ for (i in seq_along(raster_datasets)) {
   hist(raster_data, col = hist_colors[i], main = paste("Histogram of", dataset), xlab = "", ylab = "Frequency")
 }
 
+## 3.2 Set up a multi-panel plot for CHELSA data--------------------------------------------------------
 
-## 3.2  Plot histograms of GMBApoly data--------------------------------------------------------
+
+# Define colors for histograms
+hist_colors_chel<- c("#1f77b4", "#ff7f0e", "#2ca02c", "#d62728", "#9467bd", "#8c564b", "#e377c2")
+
+# Create a list of precipitation raster data sets
+precipitation_datasets <- list(
+  CCSM_Past = "Prec_CCSM_Past",
+  CNRM_Past = "Prec_CNRM_Past",
+  FGOA_Past = "Prec_FGOA_Past",
+  IPSL_Past = "Prec_IPSL_Past",
+  MIRO_Past = "Prec_MIRO_Past",
+  MPIE_Past = "Prec_MPIE_Past",
+  MRIC_Past = "Prec_MRIC_Past",
+  Pres = "Prec_Pres"
+)
+
+# Create a list of temperature raster data sets
+temperature_datasets <- list(
+  CCSM_Past = "Temp_CCSM_Past",
+  CNRM_Past = "Temp_CNRM_Past",
+  FGOA_Past = "Temp_FGOA_Past",
+  IPSL_Past = "Temp_IPSL_Past",
+  MIRO_Past = "Temp_MIRO_Past",
+  MPIE_Past = "Temp_MPIE_Past",
+  MRIC_Past = "Temp_MRIC_Past",
+  Pres = "Temp_Pres"
+)
+
+# Function to plot histograms using base R hist function
+plot_histograms_base <- function(datasets, title) {
+  par(mfrow=c(5, 2), mar=c(3,3,1,1))
+  
+  for (i in seq_along(datasets)) {
+    dataset <- datasets[[i]]
+    raster_data <- get(dataset)  # Get raster object
+    
+    # Extract raster values
+    raster_values <- getValues(raster_data)
+    
+    # Filter out NA and non-finite values
+    raster_values <- raster_values[!is.na(raster_values) & is.finite(raster_values)]
+    
+    # Plot histogram using base R hist function
+    hist(raster_values, col = hist_colors_chel[i], main = paste("Histogram of", title, "-", dataset), xlab = "Value", ylab = "Frequency")
+  }
+}
+
+# Plot precipitation histograms using base R hist function
+plot_histograms_base(precipitation_datasets, "Precipitation")
+
+# Plot temperature histograms using base R hist function
+plot_histograms_base(temperature_datasets, "Temperature")
+
+
+for (dataset_name in names(precipitation_datasets)) {
+  dataset <- get(precipitation_datasets[[dataset_name]])
+  print(summary(dataset))
+}
+
+
+## 3.2  Plot histograms of GMBApoly data--------------------------------------------------------------------------------------------------------------------------
+
 
 print(names(GMBApoly)[variablesHis])
 
-Vari_Num <- c(4, 6, 20:33, 38, 39) # Define the column indices of interest Max, mean, uplift, area
+Vari_Num <- c(4, 6, 26:41) # Define the column indices of interest Max, mean, uplift, area
 Title_var_Num <- names(GMBApoly)[Vari_Num] # Extract the column names of interest
 
 
@@ -117,7 +203,7 @@ for (i in seq_along(Vari_Num)) {
 
 
 
-## 3,3 Violin plots of GMBApoly data --------------------------------------------------------
+## 3,3 Violin plots of GMBApoly data ----------------------------------------------------------------------------------------------------------------------------
 # Reshape the data to long format
 melted_data <- gather(GMBApoly, key = "Variable", value = "Value", all_of(Vari_Num))
 melted_data$variable <- factor(melted_data$variable, levels = Vari_Num)
@@ -137,7 +223,7 @@ ggplot(melted_data, aes(x = Variable, y = Value, fill = Variable)) +
 
 
 
-## 3.4 summarizing results PCA
+## 3.4 summarizing results PCA of the M---------------------------------------------------------------------------------------------
 
 
 PCAData <- GMBApoly[,c(4, 6, 33:41)] #taking only the numeric values 
@@ -150,6 +236,9 @@ if (anyNA(PCAData)) {
 
 # Visualize PCA results
 pca_plot <- fviz_pca(res.pca, geom = "point", ggtheme = theme_minimal())
+
+pca_plot
+
 
 
 
